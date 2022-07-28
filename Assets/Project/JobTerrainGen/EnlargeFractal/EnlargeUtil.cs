2e5addf65efd7c33e7659e99e003d98953ec19d2
iamabigartist <pch4178@gmail.com>
2022-07-28 15:13:11 +0800

    enlarge stage util

---
+++ Assets/Project/JobTerrainGen/EnlargeFractal/EnlargeUtil.cs
@@ -0,0 +1,48 @@
+﻿using JobTerrainGen.EnlargeFractal.Samplers;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+namespace JobTerrainGen.EnlargeFractal
+{
+	public static class EnlargeUtil
+	{
+		public enum Stage
+		{
+			Normal,
+			Sawtooth
+		}
+
+		public static JobHandle EnlargePlan(NativeArray<int> data, int2 data_size, out NativeArray<int>[] Results, Stage[] EnlargeStages, uint rand_seed, JobHandle deps)
+		{
+			var stage_count = EnlargeStages.Length;
+			Results = new NativeArray<int>[stage_count];
+
+			var cur_data = data;
+			NativeArray<int> cur_result;
+			var cur_data_size = data_size;
+			JobHandle cur_jh = deps;
+
+			for (int i = 0; i < stage_count; i++)
+			{
+				switch (EnlargeStages[i])
+				{
+					case Stage.Normal:
+						{
+							cur_jh = Enlarge2X2<Compare11Sampler>.Plan(cur_data, cur_data_size, out cur_result, new(), rand_seed, cur_jh);
+						}
+						break;
+					case Stage.Sawtooth:
+						{
+							cur_jh = Enlarge2X2<Rand11Sampler>.Plan(cur_data, cur_data_size, out cur_result, new(), rand_seed, cur_jh);
+						}
+						break;
+					default: throw new();
+				}
+				cur_data = cur_result;
+				cur_data_size *= 2;
+				Results[i] = cur_result;
+			}
+			return cur_jh;
+		}
+	}
+}
